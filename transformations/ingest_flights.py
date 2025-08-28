7060536240f6fa3011c15d6757e8292cc313780b
harrywatts125 <harrywatts125>
2025-08-28 12:02:38 +0000

    Added height expectation

--- transformations/ingest_flights.py
+++ transformations/ingest_flights.py
@@ -4,6 +4,8 @@ spark.dataSource.register(OpenSkyDataSource)
 
 @dlt.expect("icao24_not_null", "icao24 IS NOT NULL")
 @dlt.expect_or_drop("coord_exist", "latitude IS NOT NULL AND longitude IS NOT NULL")
+@dlt.expect_or_drop("altitude_positive", "geo_altitude >= 0")
+@dlt.expect_or_drop("velocity_positive", "velocity >= 0")
 
 # declare a streaming table
 @dlt.table
