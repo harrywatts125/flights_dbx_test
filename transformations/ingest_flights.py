ee66ab1d20fdb5aa320c5ae6a2eed26e4ff4fc92
harrywatts125 <harrywatts125>
2025-08-28 10:51:23 +0000

    Added expectations

--- transformations/ingest_flights.py
+++ transformations/ingest_flights.py
@@ -2,6 +2,9 @@
 from pyspark_datasources import OpenSkyDataSource
 spark.dataSource.register(OpenSkyDataSource)
 
+@dlt.expect("icao24_not_null", "icao24 IS NOT NULL")
+@dlt.expect_or_drop("coord_exist", "latitude IS NOT NULL AND longitude IS NOT NULL")
+
 # declare a streaming table
 @dlt.table
 def ingest_flights():
